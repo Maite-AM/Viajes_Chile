54ea2f1ce19e6d525cdf80afff890750daa14109
Maite_Astete <maite.astete@gmail.com>
2022-07-23 04:11:00 -0400

    Ajustes de estilo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 $(function(){
 
+    // Código para "navegar" entre secciones
     $("a").click(function(event){
       if (this.hash !== "") {
         event.preventDefault();
@@ -16,14 +17,17 @@ $(function(){
   
     $('[data-toggle="popover"]').popover();
 
+    //Carousel
     $('.carousel').carousel();
 
+    //Mensaje tipo "ALERTA", una vez que se hace click en botón "Enviar", en sección "Contacto"
     $(document).ready(function(){
         var botonCorreo = $('#enviarMensaje');
         botonCorreo.on('click', function(){
             alert("Mensaje enviado exitosamente. Te contactaremos a la brevedad!");})
         });
-
+    
+    //Tooltip - Mensaje sobre íconos del footer
     $(document).ready(function(){
         $('[data-toggle="tooltip"]').tooltip({
             placement : 'top'
