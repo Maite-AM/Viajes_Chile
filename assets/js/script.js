a2857667826e717c03de0a1ae996e3296a02e377
Maite_Astete <maite.astete@gmail.com>
2022-07-23 02:21:21 -0400

    Actualización y ajustes de secciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,30 @@
 $(function(){
-    $('.carousel').carousel()
-})
+
+    $("a").click(function(event){
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var gato = this.hash;
+  
+        $("html, body").animate({
+          scrollTop: $(gato).offset().top
+        }, 800, function(){
+          window.location.hash = gato;
+        });
+      }
+    });
+  
+    $('[data-toggle="popover"]').popover();
+
+    $('.carousel').carousel();
+
+    $(document).ready(function(){
+        var botonCorreo = $('#enviarMensaje');
+        botonCorreo.on('click', function(){
+            alert("Mensaje enviado exitosamente. Te contactaremos a la brevedad!");})
+        });
+
+  });
 
 
-$(document).ready(function(){
-var botonCorreo = $('#enviarMensaje');
-botonCorreo.on('click', function(){
-    alert("Mensaje enviado exitosamente. Te contactaremos a la brevedad!");})
-})
+  
